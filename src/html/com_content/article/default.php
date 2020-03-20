a263bf94a1b374805b9346cff372135a7ce1d5d1
Gijs Lamon <Gileba@users.noreply.github.com>
2020-03-20 21:45:01 +0100

    last fix?

--- src/html/com_content/article/default.php
+++ src/html/com_content/article/default.php
@@ -154,9 +154,11 @@ JHtml::_('behavior.caption');
 	<?php $active = $menu->getActive(); ?>
 	<?php $itemId = $active->id; ?>
 	<?php $link = new JUri(JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId, false)); ?>
-	<?php $link->setVar('return',
+	<?php
+	$link->setVar('return',
 		base64_encode(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language))
-	); ?>
+	);
+	?>
 	<p class="readmore">
 		<a href="<?php echo $link; ?>" class="register">
 		<?php $attribs = json_decode($this->item->attribs); ?>
